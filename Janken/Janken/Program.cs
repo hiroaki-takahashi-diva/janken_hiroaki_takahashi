b849bbf319658bd30c65274259213b2fc4cbba13
hiroaki-takahashi-diva <54297395+hiroaki-takahashi-diva@users.noreply.github.com>
2019-08-20 10:57:23 +0900

    Add files via upload

---
+++ Janken/Janken/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace Janken
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1～3のいづれかを入力");
+            Console.WriteLine("1:グー");
+            Console.WriteLine("2:チョキ");
+            Console.WriteLine("3:パー");
+
+            string Player = Console.ReadLine();
+
+            check(Player);
+
+            janken(Player);
+        }
+
+        static string check(string Player)
+        {
+            // 1 2 3が入力されるまでループ
+            while (true)
+            {
+                if (Player == "1" || Player == "2" || Player == "3")
+                {
+                    return Player;
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("1～3のいづれかを入力");
+                    Console.WriteLine("1:グー");
+                    Console.WriteLine("2:チョキ");
+                    Console.WriteLine("3:パー");
+
+                    Player = Console.ReadLine();
+                }
+            }
+        }
+
+        static void janken(string Player)
+        {
+            Random ran = new Random();
+            string Computer = ran.Next(1, 4).ToString();
+
+            //負けた場合
+            if (((Player == "1") && (Computer == "3")) ||
+                ((Player == "2") && (Computer == "1")) ||
+                ((Player == "3") && (Computer == "2")))
+            {
+                if (Player == "1")
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: グー");
+                    Console.WriteLine("Computer: パー");
+                    Console.WriteLine("まけ");
+                }
+                else if (Player == "2")
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: チョキ");
+                    Console.WriteLine("Computer: グー");
+                    Console.WriteLine("まけ");
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: パー");
+                    Console.WriteLine("Computer: チョキ");
+                    Console.WriteLine("まけ");
+                }
+            }
+            //勝った場合
+            else if (((Player == "1") && (Computer == "2")) ||
+                     ((Player == "2") && (Computer == "3")) ||
+                     ((Player == "3") && (Computer == "1")))
+            {
+                if (Player == "1")
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: グー");
+                    Console.WriteLine("Computer: チョキ");
+                    Console.WriteLine("かち");
+                }
+                else if (Player == "2")
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: チョキ");
+                    Console.WriteLine("Computer: パー");
+                    Console.WriteLine("かち");
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: パー");
+                    Console.WriteLine("Computer: グー");
+                    Console.WriteLine("かち");
+                }
+            }
+            //あいこだった場合
+            else
+            {
+                if (Player == "1")
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: グー");
+                    Console.WriteLine("Computer: グー");
+                    Console.WriteLine("あいこ");
+                    Console.WriteLine(Environment.NewLine);
+                }
+                else if (Player == "2")
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: チョキ");
+                    Console.WriteLine("Computer: チョキ");
+                    Console.WriteLine("あいこ");
+                    Console.WriteLine(Environment.NewLine);
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Player: パー");
+                    Console.WriteLine("Computer: パー");
+                    Console.WriteLine("あいこ");
+                    Console.WriteLine(Environment.NewLine);
+                }
+
+                Main();
+            }
+        }
+    }
+}
---
+++ Janken2/Janken2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Janken2
+{
+    class Program
+    {
+        static string[] Handsn = { "グー", "チョキ", "パー" };
+        static void Main()
+        {
+            Console.WriteLine("プレイヤー人数を入力(1～5人)");
+            int Nplayers = int.Parse(Console.ReadLine());
+            Nplayers = Check_Nplayers(Nplayers);
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("CPU人数を入力(1～5人)");
+            int Ncomputers = int.Parse(Console.ReadLine());
+            Ncomputers = Check_Ncomputers(Ncomputers);
+            Console.WriteLine(Environment.NewLine);
+
+            int[] Players = new int[Nplayers];
+            int[] Computers = new int[Ncomputers];
+
+            for(int i = 0;i < Nplayers; i++)
+            {
+                Console.WriteLine("プレイヤー" + i);
+                Console.WriteLine("0～2のいづれかを入力");
+                Console.WriteLine("0:グー");
+                Console.WriteLine("1:チョキ");
+                Console.WriteLine("2:パー");
+                int Hand = int.Parse(Console.ReadLine());
+                Players[i] = Hand;
+                Hand = Check_Hand(Hand);
+                Console.WriteLine("プレイヤー" + i + "は" + Handsn[Hand]);
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            for(int n = 0;n < Ncomputers; n++)
+            {
+                Console.WriteLine("CPU" + n);
+                Random ran = new Random();
+                int Hand = ran.Next(3);
+                Computers[n] = Hand;
+                Console.WriteLine("CPU" + n + "は" + Handsn[Hand]);
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            int[] HandAll = new int[Players.Length + Computers.Length];
+
+            Players.CopyTo(HandAll, 0);
+            Computers.CopyTo(HandAll, Players.Length);
+
+            foreach (int a in HandAll)
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        static int Check_Nplayers(int Nplayers)
+        {
+            // 1～5が入力されるまでループ
+            while (true)
+            {
+                if (1<= Nplayers && Nplayers <= 5)
+                {
+                    return Nplayers;
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("プレイヤー人数を入力(1～5人)");
+                    Nplayers = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        static int Check_Ncomputers(int Ncomputers)
+        {
+            // 1～5が入力されるまでループ
+            while (true)
+            {
+                if (1 <= Ncomputers && Ncomputers <= 5)
+                {
+                    return Ncomputers;
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("CPU人数を入力(1～5人)");
+                    Ncomputers = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        static int Check_Hand(int Hand)
+        {
+            // 0～2が入力されるまでループ
+            while (true)
+            {
+                if (0 <= Hand && Hand <= 2)
+                {
+                    return Hand;
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("0～2のいづれかを入力");
+                    Console.WriteLine("0:グー");
+                    Console.WriteLine("1:チョキ");
+                    Console.WriteLine("2:パー");
+
+                    Hand = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+    }
+}
